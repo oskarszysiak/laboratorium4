935f7679a6c55b049c4f808ab0308004563560c5
oskarszysiak <oskszysiak@gmail.com>
2021-11-24 16:08:21 +0100

    commit

--- sprawdzian2/main.c
+++ sprawdzian2/main.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    int tab2[] = {4,3,-7,2,8,0,8,1,-2,3};
+    for(int i=0; i<10; i++)
+    {
+        printf("%d\n", tab2[9-i]);
+        if(tab2[9-i]>0 && tab2[9-i]%2==0)
+        {
+            printf("parzysta i dodatnia liczba");
+        }
+    }
 }
