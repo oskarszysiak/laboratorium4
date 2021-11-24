c16f169f3f9f567e65674f2851c88b13602f3997
oskarszysiak <oskszysiak@gmail.com>
2021-11-24 16:34:03 +0100

    3rd commit

---
+++ ćwiczenie1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printMyinfo()
+{
+    printf("Oskar Szysiak zainteresowania.......\n");
+}
+
+int main()
+{
+    printMyinfo();
+    return 0;
+}
---
+++ ćwiczenie2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int printmyage()
+{
+    int wiek=19;
+    return wiek;
+}
+int main()
+{
+   int myage=printmyage();
+   printf("Moj wiek to: %d", myage);
+    return 0;
+}
